280e66529199f16b8f1755ffc3d6475437025f07
Ryoung27 <RRYoung89@gmail.com>
2018-06-08 13:57:03 -0500

    first commit

---
+++ src/nav/NavBar.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react"
+import "./NavBar.css"
+
+export default class NavBar extends Comment {
+
+    render() {
+
+    }
+}
---
+++ src/project/ProjectList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+//import Project from "./Project";
+
+export default class ProjectList extends Component {
+
+    render() {
+        return (
+            <div className="card post">
+                <div className="card-body">
+                    <h5 className="card-text"> {this.props.projects.address} </h5>
+        )
+    }
+}
